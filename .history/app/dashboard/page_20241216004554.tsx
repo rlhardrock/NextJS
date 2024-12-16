4188de1d009994b57609202c39e95c7a75bab69a
Louis Rey <rlhardrock@gmail.com>
2024-12-16 01:02:04 -0500

    Cards & LatestInvoices components enabled

--- .history/app/dashboard/page_20241216004554.tsx
+++ .history/app/dashboard/page_20241216004554.tsx
@@ -7,12 +7,7 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
+  const cardData = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
